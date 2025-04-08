858eed510865e8e047ffa81c989552130fb7a895
Ryan Baker <rbaker@xtier.com>
2025-04-08 11:37:13 -0400

    automated commit

--- scripts/git.py
+++ scripts/git.py
@@ -7,15 +7,19 @@ rootdir = "/Users/ryanbaker/cxxsaturdays"
 
 
 if __name__ == "__main__":
-    create_backup()
+    if input("Would you like to create a local backup? [y/n] ") in "yY":
+        create_backup()
+
     try:
         if len(sys.argv) >= 2:
             commit_message = sys.argv[1]
         else:
-            commit_message = "automated commit"
+            commit_message = input("Enter a commit message: ") 
+
         subprocess.run(["git", "-C", rootdir, "add", "--all", ":!*.mp4"], check=True)
         subprocess.run(["git", "-C", rootdir, "commit", "-m", commit_message], check=True)
         subprocess.run(["git", "-C", rootdir, "push"], check=True)
+
         print()
         print("✅ Changes committed and pushed successfully")
     except Exception as e:
